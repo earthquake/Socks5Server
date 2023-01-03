0e78cb72e6f5aa09111369464e056b84d1c305ed
Balazs Bucsay <balazs.bucsay@cosaic.io>
2023-01-03 14:33:13 +0000

    usage/arguments added

--- Socks5Server/main.cpp
+++ Socks5Server/main.cpp
@@ -4,9 +4,18 @@
 #include <windows.h>
 #include "SocksServer.h"
 
-int main()
+int wmain(int argc, wchar_t* argv[])
 {
-	StartServer(L"127.0.0.1", L"1080");
+	if (argc < 3)
+	{
+		wprintf(L"Usage : .\\%s [Listening IP] [Listening Port]\r\n\r\n", argv[0]);
+		StartServer(L"127.0.0.1", L"1080");
+	}
+	else
+	{
+		StartServer(argv[1], argv[2]);
+	}
+	
 
     return 0;
 }
